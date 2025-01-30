b6107780a4a14f2e098614302f37c2517fa67f54
Raaul04 <raulletrado@hotmail.com>
2025-01-30 20:17:34 +0100

    update

--- schema.ts
+++ schema.ts
@@ -1,7 +1,23 @@
 export const schema = `#graphql
+    type Aeropuerto{
+        id:ID!,
+        iata:String!,
+        name:String!,
+        ciudad:String!,
+        pais:String!,
+        latitude:String!,
+        longitude:String!,
+        timezone:String!,
+        hora:String!,
+    }
+    type Query:{
+        getAirport(id:ID!):Aeropuerto
+        getAirports:[Aeropuerto]!
+    },
 
-    type Query{
-        default : String!
+    type Mutation{
+        addAirport(iata:String!,name:String!,ciudad:String!,pais:String!):Aeropuerto!
+
     }
 
 `;
--- types.ts
+++ types.ts
@@ -1,6 +1,21 @@
-//import { OptionalId } from "mongodb";
-/*
+import { OptionalId } from "mongodb";
+
 export type AeropuertoModel=OptionalId<{
+    iata:string,
+    name:string,
+    ciudad:string,
+    pais:string,
+    latitude:number,
+    longitude:number,
+    timezone:string,
+}>
 
-
-}>*/
+//https://api.api-ninjas.com/v1/airports
+export type API_AIRPORT={
+    latitude:number,
+    longitude:number
+}
+//https://api.api-ninjas.com/v1/timezone
+export type API_TIME={
+    hora:string
+}
